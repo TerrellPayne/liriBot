5e2c62a87e84022486353d4e9ad9bff6ed2ddc8a
terrellp <terrellpayne@Cheyennes-MacBook.local>
2019-01-18 14:40:43 -0500

    new

--- liri.js
+++ liri.js
@@ -44,11 +44,24 @@ if (input[2] === "movie-this") {
 if (input[2] === "do-what-it-says") {
 
    // read data from text file
-    fs.readFile('./random.txt', function(err, data) {
+    fs.readFile('./random.txt', "utf8", function(err, data) {
         if (err) {
             throw err;
         }
-        
+        console.log(data);
+
+        var dataArray = data.split(",");
+        console.log(dataArray);
+
+        spotify.search({ type: "track", query: dataArray[1] }, function (err, data) {
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+    
+            console.log(data.tracks.items);
+            return;
+        });
         // seperate data into command and argument
             // var command
             // var arguement
@@ -57,42 +70,7 @@ if (input[2] === "do-what-it-says") {
 
         // if command is concert
             // do the concert search thing
-        if (command === "concert-this") {
-            // console.log(input[3])
         
-            axios.get("https://rest.bandsintown.com/artists/" + input[3] + "/events?app_id=codingbootcamp")
-                .then(function (response) {
-                    console.log(response.data);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        }
-        
-        // if command is spotify
-            // do the spoitfy search
-        if (command === "spotify-this-song") {
-            spotify.search({ type: "track", query: input[3] }, function (err, data) {
-                if (err) {
-                    console.log('Error occurred: ' + err);
-                    return;
-                }
-        
-                console.log(data.tracks.items);
-                return;
-            });
-        }
-        // if commmand is movie 
-            // do the movie thing
-        if (command === "movie-this") {
-            axios.get("https://www.omdbapi.com/?t=" + input[3] + "&y=&plot=short&apikey=trilogy")
-                .then(function (response) {
-                    console.log(response.data);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        }
     });
 
 }
